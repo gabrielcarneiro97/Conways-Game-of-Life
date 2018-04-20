223732062fecdbf3aee9dbde7a94cc6d2dfe414b
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-04-20 12:56:20 -0300

    Mais algumas estruturas

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,62 @@
+use std::rc::Rc;
+
 #[derive(Debug)]
 enum State {
     Alive,
     Dead
 }
 
-struct Position {
+struct Coords {
     x: usize,
     y: usize
 }
 
 struct Cell {
     state: State,
-    position: Position
+    position: Coords,
+    neighboors: Vec<Rc<Cell>>
+}
+
+impl Cell {
+    fn new(state: State, position: Coords) -> Cell {
+        let mut this = Cell {
+            state,
+            position,
+            neighboors: Vec::new()
+        };
+
+        this.discover_neighbors();
+        
+        this
+    }
+
+    fn discover_neighbors(&mut self) {
+        
+    }
+}
+
+struct Map {
+    alives: Vec<Coords>,
+    size: Coords,
+    counter: usize
+}
+
+impl Map {
+    fn new(size: Coords) -> Map {
+        Map {
+            alives: Vec::new(),
+            size,
+            counter: 0
+        }
+    }
+
+    fn next_tick(&mut self) {
+
+    }
+
+    fn map(&self) {
+
+    }
 }
 
 fn main() {

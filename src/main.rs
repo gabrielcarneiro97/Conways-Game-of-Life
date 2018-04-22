8fbbf3b2761d629c1246d23d7fec7769a1de9600
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-04-22 14:33:46 -0300

    Tudo funcionando

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use std::rc::Rc;
 use std::cell::RefCell;
+use std::time::Duration;
+use std::thread;
 
-#[derive(Debug)]
-#[derive(PartialEq)]
+
+#[derive(PartialEq, Debug, Clone)]
 enum State {
     Alive,
     Dead
 }
 
-#[derive(Clone)]
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Coords {
     x: usize,
     y: usize
@@ -19,7 +20,8 @@ struct Coords {
 struct Cell {
     state: State,
     position: Coords,
-    neighboors: Vec<Coords>
+    neighboors: Vec<Coords>,
+    neighboors_alive: i32
 }
 
 impl Cell {
@@ -27,7 +29,8 @@ impl Cell {
         Cell {
             state,
             position,
-            neighboors
+            neighboors,
+            neighboors_alive: 0
         }
     }
 
@@ -75,7 +78,7 @@ impl Cell {
                 neighboors.push(Coords { x: x + 1, y: y - 1});
                 neighboors.push(Coords { x: x + 1, y: y});
                 neighboors.push(Coords { x: x + 1, y: y + 1});
-                neighboors.push(Coords { x: x + 1, y: y});
+                neighboors.push(Coords { x: x, y: y + 1});
             } else if x == size.x - 1 {
                 neighboors.push(Coords { x: x, y: y - 1});
                 neighboors.push(Coords { x: x - 1, y: y - 1});
@@ -123,7 +126,7 @@ impl Map {
 
     fn populate(size: &Coords) -> Vec<Rc<RefCell<Cell>>> {
 
-        let mut vec = Vec::new();
+        let mut vec : Vec<Rc<RefCell<Cell>>> = Vec::new();
 
         for x in 0..size.x {
             for y in 0..size.y {
@@ -136,11 +139,62 @@ impl Map {
     }
 
     fn next_tick(&mut self) {
+        self.counter += 1;
+
+        for cell in &self.cells {
+            let mut borrow = cell.borrow_mut();
+            let mut alives = 0;
+
+            for neighboor in &borrow.neighboors {
+                let neighboor_cell = self.get_cell(&neighboor);
+                let state = &neighboor_cell.borrow().state;
+
+                match *state {
+                    State::Alive => alives += 1,
+                    State::Dead => ()
+                }
+            }
+            borrow.neighboors_alive = alives;            
+        }
+
+        for cell in &self.cells {
+            let mut borrow = cell.borrow_mut();
+            let alives = borrow.neighboors_alive;
+
+            let state = borrow.state.clone();
+
+            match state {
+                State::Alive => {
+                    if alives < 2 || alives > 3 {
+                        borrow.change_state();
+                    }
+                },
+                State::Dead => {
+                    if alives == 3 {
+                        borrow.change_state();
+                    }
+                }
+            }
+        }
 
     }
 
     fn map(&self) {
+        let x_max = self.size.x;
+        let y_max = self.size.y;
 
+        for x in 0..x_max {
+            for y in 0..y_max {
+                let cell = self.get_cell(&Coords {x: x, y: y});
+                let state = &cell.borrow().state;
+
+                match *state {
+                    State::Alive => print!("A"),
+                    State::Dead => print!("D")
+                }
+            }
+            println!("");
+        }
     }
 
     fn get_cell(&self, coord: &Coords) -> Rc<RefCell<Cell>> {
@@ -149,9 +203,25 @@ impl Map {
         Rc::clone(&self.cells[pos])
     }
 
+    fn set_moc(&self) {
+        let cell = self.get_cell(&Coords {x: 0, y: 1});
+        cell.borrow_mut().change_state();
+        let cell = self.get_cell(&Coords {x: 1, y: 1});
+        cell.borrow_mut().change_state();
+        let cell = self.get_cell(&Coords {x: 2, y: 1});
+        cell.borrow_mut().change_state();
+    }
+
 }
 
 fn main() {
     let mut map = Map::new(Coords {x: 3, y: 3});
-    println!("{:?}", map);
+    map.set_moc();
+
+    loop {
+        thread::sleep(Duration::from_millis(1000));
+        print!("{}[2J", 27 as char);
+        map.map();
+        map.next_tick();
+    }
 }

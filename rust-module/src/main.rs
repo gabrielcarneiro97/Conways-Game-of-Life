bd0fdef334d44b310cf7a935c0d050c9a5c7969a
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-04-25 11:42:54 -0300

    Aumentei a eficiencia da função que confere se uma celulua está viva através de um BitVec

--- rust-module/src/main.rs
+++ rust-module/src/main.rs
@@ -1,5 +1,13 @@
+extern crate rand;
+extern crate time;
+extern crate bit_vec;
+
+use bit_vec::BitVec;
+
+use time::PreciseTime;
 use std::time::Duration;
 use std::thread;
+use rand::distributions::{IndependentSample, Range};
 
 
 #[derive(PartialEq, Debug, Clone)]
@@ -109,6 +117,7 @@ impl Cell {
 #[derive(Debug)]
 pub struct Map {
     alives: Vec<Coords>,
+    bit_map: BitVec,
     visible_size: Coords,
     true_size: Coords,
     offset: Coords,
@@ -120,6 +129,7 @@ impl Map {
         let true_size = Coords {x: visible_size.x * 4, y: visible_size.y * 4};
         Map {
             alives: Vec::new(),
+            bit_map: BitVec::from_elem(true_size.x * true_size.y, false),
             offset: Coords { x: true_size.x/2, y: true_size.y/2 },
             true_size,
             visible_size,
@@ -132,6 +142,7 @@ impl Map {
         self.generation += 1;
 
         let mut next_gen : Vec<Coords> = Vec::new();
+        let mut kill : Vec<usize> = Vec::new();
 
         for cell_coord in &self.alives {
             let mut cell = Cell::new(State::Alive, 
@@ -165,8 +176,20 @@ impl Map {
 
             if cell.neighboors_alive == 2 || cell.neighboors_alive == 3 {
                 next_gen.push(Coords {x: cell_coord.x, y: cell_coord.y});
+            } else {
+                let pos = cell_coord.y + (cell_coord.x * self.true_size.y);
+                kill.push(pos);
             }
 
+        }
+
+        for cell in &next_gen {
+            let pos = cell.y + (cell.x * self.true_size.y);
+            self.bit_map.set(pos, true);
+        }
+
+        for to_kill in kill {
+            self.bit_map.set(to_kill, false);
         }
 
         self.alives = next_gen;
@@ -201,20 +224,49 @@ impl Map {
     }
 
     pub fn is_alive(&self, coord: &Coords) -> bool {
-        let ret;
-        match &self.alives.iter().position(|cell| cell.x == coord.x && cell.y == coord.y) {
-            Some(_) => {ret = true;},
-            None => {ret = false;}
-        };
-        ret
+        let pos = coord.y + (coord.x * self.true_size.y);
+        self.bit_map[pos]
     }
 
     pub fn set_alive(&mut self, coords: Vec<Coords>) {
         for coord in coords {
             if !self.is_alive(&coord) {
                 self.alives.push(Coords { x: coord.x + self.offset.x, y: coord.y + self.offset.y});
+                let pos = (coord.y + self.offset.y) + ((coord.x + self.offset.x) * self.true_size.y);
+                self.bit_map.set(pos, true);
             }
         }
+    }
+    
+    pub fn set_random(&mut self) {
+
+        let max_max;
+
+        if self.visible_size.x * self.visible_size.y / 2 > 5000 {
+            max_max = 5000;
+        } else {
+            max_max = self.visible_size.x * self.visible_size.y / 2;
+        }
+
+        let max_range = Range::new(0, max_max);
+        let mut rng = rand::thread_rng();
+        let max = max_range.ind_sample(&mut rng) as usize;
+
+        let mut coords : Vec<Coords> = Vec::new();
+
+        println!("max>{}", max);
+
+        for _ in 0..max {
+            let range_x = Range::new(0usize, self.visible_size.x);
+            let range_y = Range::new(0usize, self.visible_size.y);
+
+            let x = range_x.ind_sample(&mut rng);
+            let y = range_y.ind_sample(&mut rng);
+
+            coords.push(Coords {x, y});
+        }
+
+        self.set_alive(coords);
     }
 
     pub fn blinker() -> Vec<Coords> {
@@ -286,15 +338,18 @@ impl Map {
 }
 
 fn main() {
-    let mut map = Map::new(Coords {x: 30, y: 50});
-    
-    map.set_alive(Map::gosper_glider_gun());
+    let mut map = Map::new(Coords {x: 40, y: 100});
+
+    map.set_random();
 
     loop {
-        thread::sleep(Duration::from_millis(100));
+        // thread::sleep(Duration::from_millis(1000));
         print!("{}[2J", 27 as char);
         println!("------generation({})------", map.generation);
         map.map();
+        let start = PreciseTime::now();
         map.next_tick();
+        let end = PreciseTime::now();
+        println!("-------time({})-------", start.to(end));
     }
 }

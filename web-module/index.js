d2a87a859c1c7377216e718ede13d19acba5b50e
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-05-08 15:45:51 -0300

    Adaptei o código ao Arbnb Guide Style

--- web-module/index.js
+++ web-module/index.js
@@ -1,147 +1,145 @@
-import * as PIXI from 'pixi.js'
-const wasm = import('../rust-module/pkg/conways_game_of_life')
-
-function randomColor () {
-  return parseInt(((1 << 24) * Math.random() | 0).toString(16), 16)
+import * as PIXI from 'pixi.js';
+const wasm = import('../rust-module/pkg/conways_game_of_life');
+function randomColor() {
+  return parseInt((16777216 * Math.random()).toString(16), 16);
 }
 
-const xMap = 40
-const yMap = 30
-const xTrue = xMap * 4
-const yTrue = yMap * 4
-const cellSide = 20
+const xMap = 40;
+const yMap = 30;
+const xTrue = xMap * 4;
+const yTrue = yMap * 4;
+const cellSide = 20;
 
-const deadFill = 0xF2F2F2
-const aliveFill = randomColor()
+const deadFill = 0xF2F2F2;
+const aliveFill = randomColor();
 
 class Cell {
-  constructor (x, y, width, height) {
-    this.isAlive = false
-
-    this.x = x
-    this.y = y
-    this.width = width
-    this.height = height
+  constructor(x, y, width, height) {
+    this.isAlive = false;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
 
     this.changeState = (graphics) => {
       if (this.isAlive) {
-        this.isAlive = false
-        graphics.beginFill(deadFill)
-        graphics.drawRect(x + 0.5, y + 0.5, width - 1, height - 1)
-        graphics.endFill()
+        this.isAlive = false;
+        graphics.beginFill(deadFill);
+        graphics.drawRect(x + 0.5, y + 0.5, width - 1, height - 1);
+        graphics.endFill();
       } else {
-        this.isAlive = true
-        graphics.beginFill(aliveFill)
-        graphics.drawRect(x + 0.5, y + 0.5, width - 1, height - 1)
-        graphics.endFill()
+        this.isAlive = true;
+        graphics.beginFill(aliveFill);
+        graphics.drawRect(x + 0.5, y + 0.5, width - 1, height - 1);
+        graphics.endFill();
       }
-    }
+    };
 
     this.setAlive = (graphics) => {
-      this.isAlive = true
-      graphics.beginFill(aliveFill)
-      graphics.drawRect(x + 0.5, y + 0.5, width - 1, height - 1)
-      graphics.endFill()
-    }
+      this.isAlive = true;
+      graphics.beginFill(aliveFill);
+      graphics.drawRect(x + 0.5, y + 0.5, width - 1, height - 1);
+      graphics.endFill();
+    };
 
     this.setDead = () => {
-      this.isAlive = false
-    }
+      this.isAlive = false;
+    };
   }
 }
 
-wasm.then(conways => {
+wasm.then((conways) => {
   const app = new PIXI.Application({
     width: 800.5,
-    height: 600.5
-  })
-  let map = conways.new_map(xMap, yMap)
-  document.body.appendChild(app.view)
+    height: 600.5,
+  });
+  const map = conways.new_map(xMap, yMap);
+  document.body.appendChild(app.view);
 
-  let container = new PIXI.Container()
-  container.interactive = true
+  const container = new PIXI.Container();
+  container.interactive = true;
 
-  let grid = new PIXI.Graphics()
-  grid.lineStyle(1, 0x141414)
+  const grid = new PIXI.Graphics();
+  grid.lineStyle(1, 0x141414);
 
-  let alivesGrid = new PIXI.Graphics()
-  alivesGrid.lineStyle(1, 0x141414)
+  const alivesGrid = new PIXI.Graphics();
+  alivesGrid.lineStyle(1, 0x141414);
 
-  container.addChild(grid)
-  container.addChild(alivesGrid)
-  app.stage.addChild(container)
+  container.addChild(grid);
+  container.addChild(alivesGrid);
+  app.stage.addChild(container);
 
-  grid.beginFill(deadFill)
+  grid.beginFill(deadFill);
 
-  let cellArr = []
+  const cellArr = [];
 
-  for (let x = 0; x < xMap; x++) {
-    cellArr[x] = []
-    for (let y = 0; y < yMap; y++) {
-      let cell = new Cell(x * cellSide, y * cellSide, cellSide, cellSide)
-      cellArr[x][y] = cell
+  for (let x = 0; x < xMap; x += 1) {
+    cellArr[x] = [];
+    for (let y = 0; y < yMap; y += 1) {
+      const cell = new Cell(x * cellSide, y * cellSide, cellSide, cellSide);
+      cellArr[x][y] = cell;
 
-      grid.drawRect(cell.x, cell.y, cell.width, cell.height)
+      grid.drawRect(cell.x, cell.y, cell.width, cell.height);
     }
   }
 
-  grid.endFill()
+  grid.endFill();
 
   container.on('click', (e) => {
-    let clickX = e.data.global.x
-    let clickY = e.data.global.y
+    const clickX = e.data.global.x;
+    const clickY = e.data.global.y;
 
-    let cellX = parseInt(clickX / cellSide)
-    let cellY = parseInt(clickY / cellSide)
+    const cellX = parseInt(clickX / cellSide, 10);
+    const cellY = parseInt(clickY / cellSide, 10);
 
-    let cell = cellArr[cellX][cellY]
+    const cell = cellArr[cellX][cellY];
 
-    cell.changeState(alivesGrid)
-  })
+    cell.changeState(alivesGrid);
+  });
 
-  let randomMap = () => {
-    let random = []
+  const randomMap = () => {
+    const random = [];
 
-    for (let i = 0; i < 10000; i++) {
-      let x = Math.floor(Math.random() * xTrue)
-      let y = Math.floor(Math.random() * yTrue)
+    for (let i = 0; i < 10000; i += 1) {
+      const x = Math.floor(Math.random() * xTrue);
+      const y = Math.floor(Math.random() * yTrue);
 
-      let pos = y + (x * yTrue)
-      random.push(pos)
+      const pos = y + (x * yTrue);
+      random.push(pos);
     }
 
-    return random
-  }
+    return random;
+  };
 
-  map.set_alive(randomMap())
+  map.set_alive(randomMap());
 
-  let defineMap = (prevAlives, alives) => {
-    alivesGrid.clear()
+  const defineMap = (prevAlives, alives) => {
+    alivesGrid.clear();
 
-    for (let i = 0; i < prevAlives.length; i++) {
-      let x = parseInt(prevAlives[i] / yMap)
-      let y = parseInt(prevAlives[i] % yMap)
+    for (let i = 0; i < prevAlives.length; i += 1) {
+      const x = parseInt(prevAlives[i] / yMap, 10);
+      const y = parseInt(prevAlives[i] % yMap, 10);
       if (x < xMap && y < yMap) {
-        cellArr[x][y].setDead()
+        cellArr[x][y].setDead();
       }
     }
 
-    for (let i = 0; i < alives.length; i++) {
-      let x = parseInt(alives[i] / yMap)
-      let y = parseInt(alives[i] % yMap)
+    for (let i = 0; i < alives.length; i += 1) {
+      const x = parseInt(alives[i] / yMap, 10);
+      const y = parseInt(alives[i] % yMap, 10);
 
       if (x < xMap && y < yMap) {
-        cellArr[x][y].setAlive(alivesGrid)
+        cellArr[x][y].setAlive(alivesGrid);
       }
     }
 
-    return alives
-  }
+    return alives;
+  };
 
-  let prev = defineMap([], map.get_map_alives())
+  let prev = defineMap([], map.get_map_alives());
 
   setInterval(() => {
-    map.next_tick()
-    prev = defineMap(prev, map.get_map_alives())
-  }, 300)
-})
+    map.next_tick();
+    prev = defineMap(prev, map.get_map_alives());
+  }, 300);
+});
